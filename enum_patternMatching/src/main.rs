d824e8d64b1d3ba8f528fafed42bd5991e485993
pk-kher <pradipkher3@gmail.com>
2025-08-09 14:11:56 +0530

    lean about enum/match/if let/let else

--- enum_patternMatching/src/main.rs
+++ enum_patternMatching/src/main.rs
@@ -25,23 +25,28 @@
 // }
 // #[derive(Debug)]
 // enum Message {
-//     Quit,
-//     Move { x: i32, y: i32 },
-//     Write(String),
-//     ChangeColor(i32, i32, i32),
+// Quit,
+// Move { x: i32, y: i32 },
+// Write(String),
+// ChangeColor(i32, i32, i32),
 // }
 // impl Message {
-//     fn call(&self) {
-//         match self {
-//             Message::Quit => println!("User quit"),
-//             Message::Move { x, y } => println!("move x: {} y: {}", x, y),
-//             Message::Write(msg) => println!("office IPv6 address: {}", msg),
-//             Message::ChangeColor(r, g, b) => println!("R:{} G:{} B:{}", r, g, b),
-//             // _=>println!(""),
-//             // other=>println!("{}",other),
-//             // other=>(), // nothing hapen unit tuple
-//         }
+// fn show(){
+//     println!("hllo");
+//
+// }
+
+// fn call(&self) {
+//     match self {
+//         Message::Quit => println!("User quit"),
+//         Message::Move { x, y } => println!("move x: {} y: {}", x, y),
+//         Message::Write(msg) => println!("office IPv6 address: {}", msg),
+//         Message::ChangeColor(r, g, b) => println!("R:{} G:{} B:{}", r, g, b),
+//         // _=>println!(""),
+//         // other=>println!("{}",other),
+//         // other=>(), // nothing hapen unit tuple
 //     }
+// }
 // }
 // this is inbuild enum you don't need to define this;
 // enum Option<T>{
@@ -52,7 +57,25 @@
 //     name:String,
 //     age:Option<i32>  //it's works same as ? in the type script
 // }
+
+fn if_let_and_let_else_ex() {
+    let x = 8;
+    let y: Option<i32> = None;
+    if let Some(data)=y{
+        println!("{}",x+data);
+
+    }else{
+        println!("value is not define");
+    }
+    // let Some(_data) = y else {
+    //     println!("value is not define");
+    //     panic!("error");
+    // };
+    // println!("hello");
+}
+
 fn main() {
+    if_let_and_let_else_ex();
     // let home=IpAddress::define_ip(IpAddrTypes::V6,String::from("129.168.0.1"));
     // let office=IpAddress::define_ip(IpAddrTypes::V4,String::from("129.168.0.1"));
     // println!("ip type:{:#?} address:{}",home.kind,home.address);
@@ -62,7 +85,7 @@ fn main() {
     // print_address(home);
     // print_address(office);
     // println!("{:#?} {:#?}", home, office);
-    // let m1 = Message::Quit;
+    // Message::show();
     // let m2 = Message::Move { x: 30, y: 10 };
     // let m3 = Message::Write(String::from("hello"));
     // let m4 = Message::ChangeColor(4, 6, 8);
@@ -92,7 +115,17 @@ fn main() {
     //     println!("value is None");
     // }
     //  //////////
-    demo_let_else()
+    // demo_let_else();
+    // slice_demo();
+    // let x: i8 = 5;
+    // let y: Option<i8> = Some(2);
+    // let sum=x+y; //error
+    // let sum = match y {
+    //     Some(v) => v + x,
+    //     None => 0 + x,
+    // };
+    // println!("{}", sum);
+    // struct_demo();
 }
 // fn print_address(add:IpAddrType){
 //     match add{
@@ -101,7 +134,38 @@ fn main() {
 //     }
 //
 // }
-fn demo_let_else() {
+fn struct_demo() {
+    #[derive(Debug)]
+    struct Rect {
+        width: i32,
+        height: i32,
+    }
+    impl Rect {
+        fn something(&self) -> String {
+            let data = format!("{} X {}", self.width, self.height);
+            String::from(data)
+        }
+    }
+    let foo = Rect {
+        width: 32,
+        height: 32,
+    };
+    let data = Rect::something(&foo);
+    println!("{:#?}", data);
+    println!("{:#?}", foo.something());
+}
+fn _slice_demo() {
+    let s = String::from("hello world");
+    let a: &String = &s;
+    let b: &str = a;
+    // let array:[i32;5]=[3;5];
+    // let data = &s[0..6];
+    // let data= &array[0..2];
+    println!("{:p}", a);
+    println!("{:p}", b);
+    // 0111 1111 1111 1110 1101 0011 1101 0100 1111 1111 0011 0000
+}
+fn _demo_let_else() {
     #[derive(Debug)] // so we can inspect the state in a minute
     enum UsState {
         Alabama,
@@ -123,12 +187,12 @@ fn demo_let_else() {
             }
         }
     }
-    let data=Coin::Quarter(UsState::Alaska);
-    let data1=Coin::Quarter(UsState::Alabama);
-    let data2=Coin::Dime;
-    println!("{:#?}",describe_state_quater(data)); 
-    println!("{:#?}",describe_state_quater(data1)); 
-    println!("{:#?}",describe_state_quater(data2)); 
+    let data = Coin::Quarter(UsState::Alaska);
+    let data1 = Coin::Quarter(UsState::Alabama);
+    let data2 = Coin::Dime;
+    println!("{:#?}", describe_state_quater(data));
+    println!("{:#?}", describe_state_quater(data1));
+    println!("{:#?}", describe_state_quater(data2));
     fn describe_state_quater(coin: Coin) -> Option<String> {
         // match coin{
         //     Coin::Quarter(value)=>Some(format!("{value:?}")),
@@ -137,9 +201,9 @@ fn demo_let_else() {
         let Coin::Quarter(value) = coin else {
             return None;
         };
-       if value.existed_in(1990) {
+        if value.existed_in(1990) {
             Some(format!("{value:?} is pretty old "))
-        }else{
+        } else {
             Some(format!("{value:?} new"))
         }
     }
